ebeda63683210aa74160467e5981d9e44260964b
Elodie Ferrais <elodieferrais@Elodies-MacBook-Pro.local>
2014-04-21 18:36:57 -0700

    add clipboard menu for copy/paste

--- idiomatic/src/main/java/com/eferrais/idiomatic/adapter/TranslationsListAdapter.java
+++ idiomatic/src/main/java/com/eferrais/idiomatic/adapter/TranslationsListAdapter.java
@@ -1,5 +1,8 @@
 package com.eferrais.idiomatic.adapter;
 
+import android.app.Activity;
+import android.content.ClipData;
+import android.content.ClipboardManager;
 import android.content.Context;
 import android.text.Html;
 import android.text.Spannable;
@@ -8,11 +11,15 @@ import android.text.Spanned;
 import android.text.TextUtils;
 import android.text.style.BackgroundColorSpan;
 import android.text.style.ForegroundColorSpan;
+import android.view.ActionMode;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.eferrais.idiomatic.R;
 import com.eferrais.idiomatic.UI.ClassStyle;
@@ -49,8 +56,24 @@ public class TranslationsListAdapter extends ArrayAdapter<Translation> {
             view = convertView;
         }
 
-        TextView initialTextView = (TextView) view.findViewById(R.id.translation_cell_initial_textview);
-        TextView translatedTextView = (TextView) view.findViewById(R.id.translation_cell_translated_textview);
+        final TextView initialTextView = (TextView) view.findViewById(R.id.translation_cell_initial_textview);
+        final TextView translatedTextView = (TextView) view.findViewById(R.id.translation_cell_translated_textview);
+
+        view.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                Activity activity = (Activity)context;
+                String message = "";
+                if (initialTextView != null) {
+                    message = initialTextView + "\n";
+                }
+                if (translatedTextView != null) {
+                    message += translatedTextView;
+                }
+                activity.startActionMode(new ActionBarCallBack(message));
+                return true;
+            }
+        });
 
         SpannableStringBuilder initialSpannable = new SpannableStringBuilder(getItem(position).getInitialText());
         SpannableStringBuilder translatedSpannable = new SpannableStringBuilder(getItem(position).getTranslatedText());
@@ -63,6 +86,8 @@ public class TranslationsListAdapter extends ArrayAdapter<Translation> {
 
         return view;
     }
+
+
 
     private void setSpans(SpannableStringBuilder text) {
         for (ClassStyle classStyle: ClassStyle.values()) {
@@ -85,5 +110,47 @@ public class TranslationsListAdapter extends ArrayAdapter<Translation> {
         }
 
 
+
     }
+
+    class ActionBarCallBack implements ActionMode.Callback
+    {
+        private String message;
+        ActionBarCallBack(String message) {
+            this.message = message;
+        }
+
+        @Override
+        public boolean onActionItemClicked(ActionMode mode, MenuItem item)
+        {
+            // TODO Auto-generated method stub
+            switch(item.getItemId()) {
+                case R.id.copyText:
+                    ClipboardManager clipboard = (ClipboardManager) context.getSystemService(context.CLIPBOARD_SERVICE);
+                    ClipData clip = ClipData.newPlainText("translation", message);
+                    clipboard.setPrimaryClip(clip);
+                    return true;
+            }
+            return false;
+
+        }
+
+        @Override
+        public boolean onCreateActionMode(ActionMode mode, Menu menu)
+        {
+            mode.getMenuInflater().inflate(R.menu.clipboard_menu, menu);
+            return true;
+        }
+
+        @Override
+        public void onDestroyActionMode(ActionMode mode)
+        {}
+
+        @Override
+        public boolean onPrepareActionMode(ActionMode mode, Menu menu)
+        {
+            return false;
+        }
+    }
+
 }
